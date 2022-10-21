04565c1664255e465b6b7e57b2ec08c76c8bfd56
Hibah0 <hibahahmed60@gmail.com>
2022-10-21 09:02:30 -0700

    Fixed memory leaks

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 unsigned int convert_c(va_list args, buffer_t *output,
-unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
-unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_p(va_list args, buffer_t *output,
-unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
+
 /**
  * convert_c - Converts an argument to an unsigned char and
  *             stores it to a buffer contained in a struct.
@@ -19,19 +20,23 @@ unsigned char flags, int wid, int prec, unsigned char len);
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_c(va_list args, buffer_t *output,
-unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-char c;
-unsigned int ret = 0;
-(void)prec;
-(void)len;
-c = va_arg(args, int);
-ret += print_width(output, ret, flags, wid);
-ret += _memcpy(output, &c, 1);
-ret += print_neg_width(output, ret, flags, wid);
+	char c;
+	unsigned int ret = 0;
 
-return (ret);
+	(void)prec;
+	(void)len;
+
+	c = va_arg(args, int);
+
+	ret += print_width(output, ret, flags, wid);
+	ret += _memcpy(output, &c, 1);
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
+
 /**
  * convert_percent - Stores a percent sign to a
  *                   buffer contained in a struct.
@@ -45,18 +50,22 @@ return (ret);
  * Return: The number of bytes stored to the buffer (always 1).
  */
 unsigned int convert_percent(va_list args, buffer_t *output,
-unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-char percent = '%';
-unsigned int ret = 0;
-(void)args;
-(void)prec;
-(void)len;
-ret += print_width(output, ret, flags, wid);
-ret += _memcpy(output, &percent, 1);
-ret += print_neg_width(output, ret, flags, wid);
-return (ret);
+	char percent = '%';
+	unsigned int ret = 0;
+
+	(void)args;
+	(void)prec;
+	(void)len;
+
+	ret += print_width(output, ret, flags, wid);
+	ret += _memcpy(output, &percent, 1);
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
+
 /**
  * convert_p - Converts the address of an argument to hex and
  *             stores it to a buffer contained in a struct.
@@ -70,22 +79,22 @@ return (ret);
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_p(va_list args, buffer_t *output,
-unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-char *null = "(nil)";
-unsigned long int address;
-unsigned int ret = 0;
+	char *null = "(nil)";
+	unsigned long int address;
+	unsigned int ret = 0;
 
-(void)len;
-address = va_arg(args, unsigned long int);
+	(void)len;
 
-if (address == '\0')
-return (_memcpy(output, null, 5));
+	address = va_arg(args, unsigned long int);
+	if (address == '\0')
+		return (_memcpy(output, null, 5));
 
-flags |= 32;
-ret += convert_ubase(output, address, "0123456789abcdef",
-flags, wid, prec);
-ret += print_neg_width(output, ret, flags, wid);
+	flags |= 32;
+	ret += convert_ubase(output, address, "0123456789abcdef",
+			flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
 
-return (ret);
+	return (ret);
 }
--- memory_helpers.c
+++ memory_helpers.c
@@ -13,35 +13,37 @@ buffer_t *init_buffer(void);
  *
  * Return: The number of bytes copied.
  */
-
 unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
 {
-unsigned int index;
-for (index = 0; index < n; index++)
-{
-*(output->buffer) = *(src + index);
-(output->len)++;
-if (output->len == 1024)
-{
-write(1, output->start, output->len);
-output->buffer = output->start;
-output->len = 0;
-}
-else
-(output->buffer)++;
-}
-return (n);
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+	{
+		*(output->buffer) = *(src + index);
+		(output->len)++;
+
+		if (output->len == 1024)
+		{
+			write(1, output->start, output->len);
+			output->buffer = output->start;
+			output->len = 0;
+		}
+
+		else
+			(output->buffer)++;
+	}
+
+	return (n);
 }
 
 /**
  * free_buffer - Frees a buffer_t struct.
  * @output: The buffer_t struct to be freed.
  */
-
 void free_buffer(buffer_t *output)
 {
-free(output->start);
-free(output);
+	free(output->start);
+	free(output);
 }
 
 /**
@@ -51,20 +53,21 @@ free(output);
  */
 buffer_t *init_buffer(void)
 {
-buffer_t *output;
-output = malloc(sizeof(buffer_t));
-if (output == NULL)
-return (NULL);
+	buffer_t *output;
 
-output->buffer = malloc(sizeof(char) * 1024);
-if (output->buffer == NULL)
-{
-free(output);
-return (NULL);
-}
+	output = malloc(sizeof(buffer_t));
+	if (output == NULL)
+		return (NULL);
 
-output->start = output->buffer;
-output->len = 0;
+	output->buffer = malloc(sizeof(char) * 1024);
+	if (output->buffer == NULL)
+	{
+		free(output);
+		return (NULL);
+	}
 
-return (output);
+	output->start = output->buffer;
+	output->len = 0;
+
+	return (output);
 }
